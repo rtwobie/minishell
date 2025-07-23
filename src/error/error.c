512b5b7025587b4628de5e1896e7d61951c6627e
R2B <robeats.hale@gmail.com>
2025-07-23 19:44:07 +0200

    feat: error messages print out "-minishell: ..." as well now

--- src/error/error.c
+++ src/error/error.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 17:46:01 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/04 19:59:41 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/07/23 17:55:11 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ char	*_lookup_err(int err)
 
 void	print_error(int err)
 {
+	ft_putstr_fd("-minishell: ", STDERR_FILENO);
 	ft_putstr_fd(_lookup_err(err), STDERR_FILENO);
 	write(STDERR_FILENO, "\n", 1);
 }
