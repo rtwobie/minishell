74dd33202aaf7640603e847f0cec481e02057dda
R2B <robeats.hale@gmail.com>
2025-07-31 15:28:28 +0200

    feat: better error printing

--- src/error/error.c
+++ src/error/error.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 17:46:01 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/23 17:55:11 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/07/31 15:21:37 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,20 @@ void	print_error(int err)
 {
 	ft_putstr_fd("-minishell: ", STDERR_FILENO);
 	ft_putstr_fd(_lookup_err(err), STDERR_FILENO);
+	write(STDERR_FILENO, "\n", 1);
+}
+
+void	print_err(int err, char *location, char *strerror)
+{
+	if (err == 0)
+		return ;
+	if (err < 0)
+		ft_putstr_fd("-minishell: ", STDERR_FILENO);
+	ft_putstr_fd(location, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	if (err < 0)
+		ft_putstr_fd(_lookup_err(err), STDERR_FILENO);
+	else if (err > 0)
+		ft_putstr_fd(strerror, STDERR_FILENO);
 	write(STDERR_FILENO, "\n", 1);
 }
--- src/error/error.h
+++ src/error/error.h
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 19:53:50 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/04 19:57:47 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/07/31 15:10:59 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 // ****************************** //
 
 void	print_error(int err);
+void	print_err(int err, char *location, char *strerror);
 
 #endif // !ERROR_H
 
