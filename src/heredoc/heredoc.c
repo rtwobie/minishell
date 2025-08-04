9237e47cd61e9d86ad5c8e2d044392a22fa6e7fa
R2B <robeats.hale@gmail.com>
2025-08-04 19:04:02 +0200

    feat: heredoc (expansion functionality excluded)

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,21 +6,54 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 16:07:09 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/30 17:42:58 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/08/04 18:59:59 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
 #include "libft.h"
 #include "tokenizer.h"
+#include "heredoc.h"
 
 // TODO: SIGNAL HANDLING
-int	heredoc(char *delimiter)
-{
-	char	*input;
+// TODO: env expansion in heredoc
 
+static char	*_generate_name(int count)
+{
+	char		*acount;
+	char		*tmpfile;
+
+	acount = ft_itoa(count);
+	if (!acount)
+		return (perror("malloc"), NULL);
+	tmpfile = ft_strjoin(HDOCDIR, acount);
+	if (!tmpfile)
+	{
+		free(acount);
+		return (perror("malloc"), NULL);
+	}
+	free(acount);
+	return (tmpfile);
+}
+
+static char	*heredoc(char *delimiter, int count)
+{
+	int		fd;
+	char	*input;
+	char	*tmpfile;
+
+	tmpfile = _generate_name(count);
+	if (!tmpfile)
+		return (NULL);
+	fd = open(tmpfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd == -1)
+		return (perror("heredoc"), NULL);
 	while (1)
 	{
 		input = readline("> ");
@@ -29,21 +62,50 @@ int	heredoc(char *delimiter)
 			free(input);
 			break ;
 		}
+		// input = envvar();
+		ft_putendl_fd(input, fd);
 		free(input);
 	}
-	printf("DELIMITER DETECTED!\n");
-	return (EXIT_SUCCESS);
+	close(fd);
+	return (tmpfile);
 }
 
 int	heredoc_test(t_token **tokens)
 {
+	int		count;
+	char	*hdoc;
 	t_token	*current;
+
+	count = 0;
+	current = *tokens;
+	while (current)
+	{
+		if (current->type != TOKEN_HERE_DOC)
+		{
+			current = current->next;
+			continue ;
+		}
+		hdoc = heredoc(current->value, count);
+		if (!hdoc)
+			return (EXIT_FAILURE);
+		free(current->value);
+		current->value = hdoc;
+		++count;
+		current = current->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	cleanup_hdoc(t_token **tokens)
+{
+	t_token *current;
 
 	current = *tokens;
 	while (current)
 	{
 		if (current->type == TOKEN_HERE_DOC)
-			heredoc(current->value);
+			if (unlink(current->value))
+				return (perror("unlink"), EXIT_FAILURE);
 		current = current->next;
 	}
 	return (EXIT_SUCCESS);
--- src/heredoc/heredoc.h
+++ src/heredoc/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:09:07 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/30 17:44:17 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/08/04 19:00:37 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 #ifndef HEREDOC_H
 # define HEREDOC_H
 
+# define HDOCDIR "./minihdoc_"
+
 typedef struct s_token t_token;
 
 int	heredoc_test(t_token **tokens);
+int	cleanup_hdoc(t_token **tokens);
 
 #endif // !HEREDOC_H
