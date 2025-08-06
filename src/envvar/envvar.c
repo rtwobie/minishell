9a0e01f592baa197244406cb79bc85ba54b60f1f
fgroo <faris.goerlich@gmail.com>
2025-08-07 00:49:16 +0200

    fix : mem leak 0

--- src/envvar/envvar.c
+++ src/envvar/envvar.c
@@ -85,9 +85,9 @@ static int	_get_env_tok(char **idx)
 static int	dollars(t_token **tokens, unsigned int *skip,
 	unsigned int *i, unsigned char *exit_status)
 {
-	const int numerical_value = *exit_status;
-	const char *extracted_str = ft_itoa(numerical_value);
-	char	*pos[3];
+	const int	numerical_value = *exit_status;
+	char		*extr_str;
+	char		*pos[3];
 
 	*i = 0;
 	if ((*tokens)->type != TOKEN_DOUBLE_QUOTES
@@ -99,11 +99,11 @@ static int	dollars(t_token **tokens, unsigned int *skip,
 		++(*skip);
 	if ((*tokens)->value[*skip] == '$' && (*tokens)->value[*skip + 1] == '?')
 	{
-		pos[0] = ft_strjoin(extracted_str,(*tokens)->value + *skip + 2);
+		extr_str = ft_itoa(numerical_value);
+		pos[0] = ft_strjoin(extr_str, (*tokens)->value + *skip + 2);
 		pos[1] = ft_substr((*tokens)->value, 0, *skip);
-		pos[2] = ft_strjoin(pos[1], pos[0]);
-		(free((*tokens)->value), free(pos[0]), free(pos[1]), (*tokens)->value = NULL);
-		(*tokens)->value = pos[2];
+		(free((*tokens)->value), pos[2] = ft_strjoin(pos[1], pos[0]));
+		(free(*pos), free(pos[1]), free(extr_str), (*tokens)->value = pos[2]);
 	}
 	while ((*tokens)->value[*skip + *i] && (*tokens)->value[*skip + *i] != '$')
 		++(*i);
--- src/envvar/envvar.h
+++ src/envvar/envvar.h
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.de>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 22:39:38 by fgorlich          #+#    #+#             */
-/*   Updated: 2025/08/06 15:42:58 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/08/07 00:37:10 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@
 # include "tokenizer.h"
 
 int	envvar(t_token **tokens, unsigned char *exit_status, unsigned int skip);
-
 
 #endif
