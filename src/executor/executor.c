5e0e1fbaea481e165e629e2c891e0a03d9c7891e
R2B <robeats.hale@gmail.com>
2025-08-07 19:21:07 +0200

    fix: concurrent fork(); feat: builtins

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.de>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 14:07:47 by admin             #+#    #+#             */
-/*   Updated: 2025/08/05 12:10:25 by fgroo            ###   ########.fr       */
+/*   Updated: 2025/08/06 20:25:03 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,45 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+#include "builtin.h"
 #include "parser.h"
 #include "executor_internal.h"
 #include "executor.h"
 
-static	unsigned char	_get_exit_status(pid_t pid)
+static int	_get_exit_status(pid_t pid)
 {
-	unsigned char	exit_status;
-	int				status;
+	int	status;
 
-	exit_status = 0;
+	status = 0;
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
-		exit_status = (unsigned char)WEXITSTATUS(status);
+		return (WEXITSTATUS(status));
 	else if (WIFSIGNALED(status))
-		exit_status = (unsigned char)WTERMSIG(status) + 128;
-	return (exit_status);
+		return (WTERMSIG(status) + 128);
+	return (status);
 }
 
-static int	_exec_cmd(t_command_node *cmd, unsigned char *exit_status,
-int fd_in, int fd_out)
+static int	_exec_builtin(t_command_node *cmd, int fd_in, int fd_out)
+{
+	int	status;
+
+	printf("EXECUTING BUILTIN...\n"); // debug
+	status = 0;
+	if (redirect_io(cmd, fd_in, fd_out))
+		return (EXIT_FAILURE);
+	if (!ft_strcmp(cmd->program_argv[0], "echo"))
+		status = echo(cmd->program_argv);
+	return (status);
+}
+
+static int	_exec_cmd(t_command_node *cmd, int fd_in, int fd_out)
 {
 	pid_t	pid;
 	char	*program;
 
 	program = NULL;
-	// if (is_builtin(cmd->program_argv[0]))
-	// 	return (_exec_builtin());
+	if (is_builtin(cmd->program_argv[0]))
+		return (_exec_builtin(cmd, fd_in, fd_out));
 	if (cmd->program_argv[0])
 	{
 		program = search_program(cmd->program_argv[0]);
@@ -62,39 +74,74 @@ int fd_in, int fd_out)
 		if (execve(program, cmd->program_argv, NULL))
 			(free(program), exit(127));
 	}
-	*exit_status = _get_exit_status(pid);
-	return (free(program), EXIT_SUCCESS);
+	return (free(program), _get_exit_status(pid));
 }
 
-static int	_exec_node(t_ast_node *node, unsigned char *exit_status,
-int fdin, int fdout)
-{
-	int		pipefd[2];
+static int	_handle_pipe(t_ast_node *node, int fdin, int fdout);
 
+static int	_exec(t_ast_node *node, int fdin, int fdout)
+{
 	if (node == NULL)
 		return (EXIT_FAILURE);
 	if (node->type == NODE_TYPE_COMMAND)
-	{
-		if (_exec_cmd(node->data.command_node, exit_status, fdin, fdout))
-			return (EXIT_FAILURE);
-	}
+		return (_exec_cmd(node->data.command_node, fdin, fdout));
 	else if (node->type == NODE_TYPE_PIPE)
+		return (_handle_pipe(node, fdin, fdout));
+	return (EXIT_FAILURE);
+}
+
+static int	_handle_pipe(t_ast_node *node, int fdin, int fdout)
+{
+	pid_t	pid[2];
+	int		pfd[2];
+	int		st;
+
+	if (pipe(pfd))
+		return (EXIT_FAILURE);
+	pid[0] = fork();
+	if (pid[0] < 0)
+		return (close(pfd[0]), close(pfd[1]), EXIT_FAILURE);
+	else if (pid[0] == 0)
 	{
-		pipe(pipefd);
-		_exec_node(node->data.pipe_node->left, exit_status, fdin, pipefd[1]);
-		close(pipefd[1]);
-		_exec_node(node->data.pipe_node->right, exit_status, pipefd[0], fdout);
-		close(pipefd[0]);
+		(close(pfd[0]), st = _exec(node->data.pipe_node->left, fdin, pfd[1]));
+		(rl_clear_history(), exit(st));
 	}
-	return (EXIT_SUCCESS);
+	pid[1] = fork();
+	if (pid[1] < 0)
+		return (close(pfd[0]), close(pfd[1]), EXIT_FAILURE);
+	else if (pid[1] == 0)
+	{
+		(close(pfd[1]), st = _exec(node->data.pipe_node->right, pfd[0], fdout));
+		(rl_clear_history(), exit(st));
+	}
+	(close(pfd[0]), close(pfd[1]));
+	return (waitpid(pid[0], NULL, 0), _get_exit_status(pid[1]));
 }
 
 int	executor(t_ast_node **tree, unsigned char *exit_status, char **envp)
 {
+	int	fd[2];
+
 	(void)envp;
 	if (!tree || !*tree)
 		return (EXIT_FAILURE);
-	if (_exec_node(*tree, exit_status, STDIN_FILENO, STDOUT_FILENO))
+	fd[0] = dup(STDIN_FILENO);
+	fd[1] = dup(STDOUT_FILENO);
+	if (fd[0] < 0 || fd[1] < 0)
+	{
+		(close(fd[0]), close(fd[1]), perror("dup failed"));
 		return (EXIT_FAILURE);
+	}
+	*exit_status = (unsigned char)_exec(*tree, STDIN_FILENO, STDOUT_FILENO);
+	if (dup2(fd[0], STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
+	{
+		(close(fd[0]), close(fd[1]), perror("dup2 failed"));
+		return (EXIT_FAILURE);
+	}
+	if (*exit_status != 0)
+	{
+		(close(fd[0]), close(fd[1]));
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
