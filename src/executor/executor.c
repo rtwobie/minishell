0273a14c0696e364f520cf1d954fa81efcb4ade9
R2B <robeats.hale@gmail.com>
2025-08-07 23:03:48 +0200

    feat: passing envp

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -36,10 +36,12 @@ static int	_get_exit_status(pid_t pid)
 	return (status);
 }
 
-static int	_exec_builtin(t_command_node *cmd, int fd_in, int fd_out)
+static int	_exec_builtin(t_command_node *cmd, int fd_in, int fd_out,
+char **envp)
 {
 	int	status;
 
+	(void)envp;
 	printf("EXECUTING BUILTIN...\n"); // debug
 	status = 0;
 	if (redirect_io(cmd, fd_in, fd_out))
@@ -49,14 +51,14 @@ static int	_exec_builtin(t_command_node *cmd, int fd_in, int fd_out)
 	return (status);
 }
 
-static int	_exec_cmd(t_command_node *cmd, int fd_in, int fd_out)
+static int	_exec_cmd(t_command_node *cmd, int fd_in, int fd_out, char **envp)
 {
 	pid_t	pid;
 	char	*program;
 
 	program = NULL;
 	if (is_builtin(cmd->program_argv[0]))
-		return (_exec_builtin(cmd, fd_in, fd_out));
+		return (_exec_builtin(cmd, fd_in, fd_out, envp));
 	if (cmd->program_argv[0])
 	{
 		program = search_program(cmd->program_argv[0]);
@@ -77,20 +79,20 @@ static int	_exec_cmd(t_command_node *cmd, int fd_in, int fd_out)
 	return (free(program), _get_exit_status(pid));
 }
 
-static int	_handle_pipe(t_ast_node *node, int fdin, int fdout);
+static int	_handle_pipe(t_ast_node *node, int fdin, int fdout, char **envp);
 
-static int	_exec(t_ast_node *node, int fdin, int fdout)
+static int	_exec(t_ast_node *node, int fdin, int fdout, char **envp)
 {
 	if (node == NULL)
 		return (EXIT_FAILURE);
 	if (node->type == NODE_TYPE_COMMAND)
-		return (_exec_cmd(node->data.command_node, fdin, fdout));
+		return (_exec_cmd(node->data.command_node, fdin, fdout, envp));
 	else if (node->type == NODE_TYPE_PIPE)
-		return (_handle_pipe(node, fdin, fdout));
+		return (_handle_pipe(node, fdin, fdout, envp));
 	return (EXIT_FAILURE);
 }
 
-static int	_handle_pipe(t_ast_node *node, int fdin, int fdout)
+static int	_handle_pipe(t_ast_node *node, int fdin, int fdout, char **envp)
 {
 	pid_t	pid[2];
 	int		pfd[2];
@@ -103,7 +105,7 @@ static int	_handle_pipe(t_ast_node *node, int fdin, int fdout)
 		return (close(pfd[0]), close(pfd[1]), EXIT_FAILURE);
 	else if (pid[0] == 0)
 	{
-		(close(pfd[0]), st = _exec(node->data.pipe_node->left, fdin, pfd[1]));
+		(close(pfd[0]), st = _exec(node->data.pipe_node->left, fdin, pfd[1], envp));
 		(rl_clear_history(), exit(st));
 	}
 	pid[1] = fork();
@@ -111,7 +113,7 @@ static int	_handle_pipe(t_ast_node *node, int fdin, int fdout)
 		return (close(pfd[0]), close(pfd[1]), EXIT_FAILURE);
 	else if (pid[1] == 0)
 	{
-		(close(pfd[1]), st = _exec(node->data.pipe_node->right, pfd[0], fdout));
+		(close(pfd[1]), st = _exec(node->data.pipe_node->right, pfd[0], fdout, envp));
 		(rl_clear_history(), exit(st));
 	}
 	(close(pfd[0]), close(pfd[1]));
@@ -122,7 +124,6 @@ int	executor(t_ast_node **tree, unsigned char *exit_status, char **envp)
 {
 	int	fd[2];
 
-	(void)envp;
 	if (!tree || !*tree)
 		return (EXIT_FAILURE);
 	fd[0] = dup(STDIN_FILENO);
@@ -132,7 +133,7 @@ int	executor(t_ast_node **tree, unsigned char *exit_status, char **envp)
 		(close(fd[0]), close(fd[1]), perror("dup failed"));
 		return (EXIT_FAILURE);
 	}
-	*exit_status = (unsigned char)_exec(*tree, STDIN_FILENO, STDOUT_FILENO);
+	*exit_status = (unsigned char)_exec(*tree, STDIN_FILENO, STDOUT_FILENO, envp);
 	if (dup2(fd[0], STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
 	{
 		(close(fd[0]), close(fd[1]), perror("dup2 failed"));
