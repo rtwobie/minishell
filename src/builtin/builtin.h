71e86bdbfaf5a8b0683126eb023bee56cd5f403b
R2B <robeats.hale@gmail.com>
2025-08-07 19:22:27 +0200

    feat: echo

--- src/builtin/builtin.h
+++ src/builtin/builtin.h
@@ -6,15 +6,15 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 17:23:34 by rha-le            #+#    #+#             */
-/*   Updated: 2025/07/04 20:00:38 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/08/06 19:04:25 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
-# include "libft.h"
-
 int	builtin_exit(int argc, char **argv);
+int	echo(char *argv[]);
+int	is_builtin(char *program);
 
 #endif // !BUILTIN_H
